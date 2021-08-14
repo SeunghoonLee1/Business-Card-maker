c8a9a70e9aaab1f127198578006d2a9ffb5ab4fe
Danny <seunghoonlee95@gmail.com>
2021-08-14 15:59:24 +0900

    project st up & add firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,13 @@
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_DB_URL,
+    databaseURL: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: "business-card-maker-768ea.appspot.com",
+    // messagingSenderId: "871676080405",
+    // appId: "1:871676080405:web:61d8fbbca89c7ad0eaf91c",
+    // measurementId: "G-BPXJM2DL20"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // firebase.analytics();
